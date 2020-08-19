c65318b8b47adcc4ac6e72e8e7c63aa0e140ea37
Sophia Hammer <sosohammer@yahoo.de>
2020-08-19 16:53:52 +0200

    tried tzhe bonus :)

--- js/index.js
+++ js/index.js
@@ -1,23 +1,37 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+ 
+  const quantity = product.querySelector('.quantity input').value;
+
+  const subtotalPrice = (price * quantity);  
+
+  let subTotal = product.querySelector('.subtotal span');
+  subTotal.innerText = `${subtotalPrice}`
+  return subtotalPrice;
+
+
 }
 
+
+
+
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let total = 0;
+  const allProducts = document.querySelectorAll('.product').forEach(function(product){
+    total+= updateSubtotal(product);
+    return total 
+  });
+  console.log(total);
+  
 
   // ITERATION 3
-  //... your code goes here
+  
+  document.querySelector('#total-value span').innerHTML = total
 }
 
 // ITERATION 4
@@ -25,18 +39,61 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const Parent = target.parentNode.parentNode;
+  Parent.remove();
+
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+ 
+  const productName = document.getElementById('newProductName');
+  const productPrice = document.getElementById('newProductPrice');
+
+  const newProductRow = document.insertRow(-1);
+
+  function addRow(tableID) {
+    // Get a reference to the table
+    let tableRef = document.getElementById(tableID);
+  
+    // Insert a row at the end of the table
+    let newRow = tableRef.insertRow(-1);
+  
+    // Insert a cell in the row at index 0
+    let newCellone = newRow.insertCell(0);
+    let newCelltwo = newRow.insertCell(1);
+    // Append a text node to the cell
+    productName = document.createTextNode('New bottom row');
+    newCellone.appendChild(productName);
+
+    productPrice = document.createTextNode('New bottom row');
+    newCelltwo.appendChild(productPrice);
+  }
+  // Call addRow() with the table's ID
+  addRow('cart');
+
+
+
+  //newProductRow.innerHTML = productName + productPrice;
+
 }
+
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeButton = document.querySelectorAll('.btn-remove').forEach(function(button){
+    button.addEventListener('click', removeProduct);
+  });
+
+
+ const createProduct = document.getElementById('create');
+ createProduct.addEventListener('click', createProduct);
+
 });
+
